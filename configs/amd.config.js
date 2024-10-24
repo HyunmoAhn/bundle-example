d44f6061696345326816a02d7a6a58046b2120f4
Ahn Hyunmo[ 안현모 ] <hyunmo.ahn@linecorp.com>
2024-10-24 23:15:02 +0900

    feat: amd support

--- configs/amd.config.js
+++ configs/amd.config.js
@@ -1,5 +1,20 @@
 const html = require("@rollup/plugin-html");
 
+const template = (script) => {
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rollup Bundle</title>
+  </head>
+  <body>
+    ${script}
+  </body>
+  </html>`;
+};
+
 /**
  * @type {import('rollup').RollupOptions}
  */
@@ -9,5 +24,19 @@ module.exports = {
     file: "output/amd/main.js",
     format: "amd",
   },
-  plugins: [html()],
+  plugins: [
+    html({
+      template: ({ files }) => {
+        const requireJsCDN =
+          "https://requirejs.org/docs/release/2.3.7/minified/require.js";
+        const scripts = files.js
+          .map(({ fileName }) => {
+            return `<script data-main="${fileName}" src="${requireJsCDN}"></script>`;
+          })
+          .join("\n");
+
+        return template(scripts);
+      },
+    }),
+  ],
 };
